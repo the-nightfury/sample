95af33f92092a88b3f76c998f3744d68a8ef3648
the-nightfury <mathewssam97@gmail.com>
2018-02-14 22:43:11 +0530

    Update sampletest.java

--- src/test/java/Sample/Junit/sampletest.java
+++ src/test/java/Sample/Junit/sampletest.java
@@ -6,12 +6,19 @@ import org.junit.Test;
 
 public class sampletest {
 
-	
 	@Test
-	public void sampletest() {
+	public void testcasepass() {
 		
 		int a =1;
 		int b = 2;
 		assertTrue(a+b == 3);
+	}
+	
+	@Test
+	public void testcasefail() {
+		
+		int a =1;
+		int b = 2;
+		assertTrue(a+b == 4);
 	}
 }
